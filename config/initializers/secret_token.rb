485e8bc43a8ba757e9e14abf4d87990d56bc7f14
Philip Mark M. Deazeta <pdeazeta@morphlabs.com>
2013-01-24 21:28:15 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LocalizationTool::Application.config.secret_token = '85604207e8dc397e196ada4ccd6b44a2d0dddd0d7f21bae1016069727049e387c15f7a35c5c5f00886f7fb4360e967dfb3f0916c94b75b36585518957a2b9df0'
